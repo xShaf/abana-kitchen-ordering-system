65f38d8e158d2aa3e69ff767696afa9610fa5775
xShaf <xxshafiqhaikalxx@gmail.com>
2024-07-06 02:06:33 +0800

    changed some text here

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <div class="fixed-bottom">
-    <p>&copy; <?php echo date('Y'); ?> Abana's Kitchen. All rights reserved.</p>
+    <p>&copy; <?php echo date('Y'); ?> Abana Kitchen. All rights reserved.</p>
 </div>
